43c730f0759e229035b8d85449af8b0127615413
Cory Crowley <t-cocrow@microsoft.com>
2018-08-12 20:05:02 -0700

    Added walls, background, and player paddles

---
+++ Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+
+    private BoxCollider2D playerCollider;
+    private enum playerState { free, wallContactTop, wallContactBottom };
+    private playerState state;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerCollider = GetComponent<BoxCollider2D>();
+        state = playerState.free;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check for keyboard input
+        if (Input.GetKey(KeyCode.S) && state != playerState.wallContactTop)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, -10f));
+        }
+        else if (Input.GetKey(KeyCode.W) && state != playerState.wallContactBottom)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 10f));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("Collision Detected");
+        if (coll.gameObject.tag == "wallTop")
+        {
+            state = playerState.wallContactTop;
+        } else if(coll.gameObject.tag == "wallBottom")
+        {
+            state = playerState.wallContactBottom;
+        }
+    }
+
+}
